c0262017413b7c6c6be18bce5b069350bda44162
Gukhwa Jeon <g.jeon3743@gmail.com>
2019-09-23 02:27:46 +0200

    registered broadcast receiver implemented

--- app/src/main/java/ch/teko/broadcastrecieverexample/MainActivity.java
+++ app/src/main/java/ch/teko/broadcastrecieverexample/MainActivity.java
@@ -3,8 +3,10 @@ package ch.teko.broadcastrecieverexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -30,6 +32,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.ConnectException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.UnknownHostException;
@@ -38,12 +41,13 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity {
     private final String LOG_TAG = "MainActivity";
     private final String api_url = "http://172.20.10.7:8080/api";
-    Intent connIntent;
+    private final int RELOADING_TIME_IN_MILLIS = 5000;
     private boolean isConnected = false;
-    private boolean isAppStopped = false;
+    private boolean isDisplayOn = false;
     private String shoppingListJSON = "";
     private EditText editText_new_item;
     private String newItem;
+    private BroadcastReceiver connectivityReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,10 +55,33 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        isAppStopped = false;
-        Log.d(LOG_TAG, "isAppStopped: " + isAppStopped);
+
+        connectivityReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                try {
+                    Log.d(LOG_TAG, intent.getAction());
+                    Log.d(LOG_TAG, ConnectivityManager.CONNECTIVITY_ACTION);
+
+                    if (intent.getAction().equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
+                        isConnected = isNetworkInterfaceAvailable(context);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            private boolean isNetworkInterfaceAvailable(Context context) {
+                final ConnectivityManager connMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo activeNetwork = connMgr.getActiveNetworkInfo();
+                return (activeNetwork != null && activeNetwork.isConnected());
+            }
+        };
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(connectivityReceiver, filter);
 
         editText_new_item = findViewById(R.id.editText_new_item);
+
         // hide keyboard when not focused on editText_new_item
         editText_new_item.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
@@ -65,157 +92,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        if (isNetworkInterfaceAvailable(this.getApplicationContext())) {
-            isConnected = true;
-            showInternet();
-            Log.d(LOG_TAG, "isConnected: " + isConnected);
-            runReloadThread();
-        } else {
-            isConnected = false;
-            showNoInternet();
-            Log.d(LOG_TAG, "isConnected: " + isConnected);
-            runReloadThread();
-        }
-
-        connIntent = getIntent();
-        if (connIntent != null) {
-            Log.d(LOG_TAG, "intent: " + connIntent.getStringExtra("connIntent"));
-        }
-
         Log.d(LOG_TAG, "onCreate() ended");
     }
 
     @Override
     protected void onStart() {
         Log.d(LOG_TAG, "onStart() called");
-        isAppStopped = false;
-        Log.d(LOG_TAG, "isAppStopped: " + isAppStopped);
         super.onStart();
+        isDisplayOn = true;
+        runReloadThread();
         Log.d(LOG_TAG, "onStart() ended");
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        Log.d(LOG_TAG, "onRestoreInstanceState() called");
-        super.onRestoreInstanceState(savedInstanceState);
-        Log.d(LOG_TAG, "onRestoreInstanceState() ended");
-    }
-
-    @Override
-    protected void onResume() {
-        Log.d(LOG_TAG, "onResume() called");
-        super.onResume();
-        Log.d(LOG_TAG, "onResume() ended");
-    }
-
-    @Override
-    protected void onPause() {
-        Log.d(LOG_TAG, "onPause() called");
-        super.onPause();
-        Log.d(LOG_TAG, "onPause() ended");
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        Log.d(LOG_TAG, "onSaveInstanceState() called");
-        super.onSaveInstanceState(outState);
-        Log.d(LOG_TAG, "onSaveInstanceState() ended");
-    }
-
-    @Override
-    protected void onStop() {
-        Log.d(LOG_TAG, "onStop() called");
-        super.onStop();
-        isAppStopped = true;
-        Log.d(LOG_TAG, "isAppStopped: " + isAppStopped);
-        Log.d(LOG_TAG, "onStop() ended");
-    }
-
-    // OnStop() -> OnRestart() -> OnStart()
-    // or OnStop() -> OnDestory()
-    @Override
-    protected void onRestart() {
-        Log.d(LOG_TAG, "onRestart() called");
-        super.onRestart();
-        Log.d(LOG_TAG, "onRestart() ended");
-    }
-
-    @Override
-    protected void onDestroy() {
-        Log.d(LOG_TAG, "onDestroy() called");
-        super.onDestroy();
-        Log.d(LOG_TAG, "onDestroy() ended");
-    }
-
-    public void onButtonAddClicked(View v) {
-        editText_new_item.clearFocus();
-        newItem = editText_new_item.getText().toString();
-        editText_new_item.setText("");
-
-        Thread communicationThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    new URL(api_url + "/?command=insert&name=" + newItem).openStream();
-
-                } catch (UnknownHostException e) {
-                    e.printStackTrace();
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        communicationThread.start();
-
-        // *** wait until the communicationThread to get transportsJsonStr from url
-        try {
-            communicationThread.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        loadTable();
-    }
-
-    public void onButtonDeleteClicked(View v) {
-        Thread communicationThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    new URL(api_url + "/?command=delete").openStream();
-
-                } catch (UnknownHostException e) {
-                    e.printStackTrace();
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        communicationThread.start();
-
-        // *** wait until the communicationThread to get transportsJsonStr from url
-        try {
-            communicationThread.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        loadTable();
-    }
-
-    public void hideKeyboard(View view) {
-        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
-        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
-    }
-
-    private boolean isNetworkInterfaceAvailable(Context context) {
-        final ConnectivityManager connMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo activeNetwork = connMgr.getActiveNetworkInfo();
-        return (activeNetwork != null && activeNetwork.isConnected());
     }
 
     private void runReloadThread() {
@@ -228,7 +114,6 @@ public class MainActivity extends AppCompatActivity {
                             runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
-                                    showInternet();
                                     loadTable();
                                 }
                             });
@@ -236,15 +121,13 @@ public class MainActivity extends AppCompatActivity {
                             runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
-                                    showNoInternet();
+                                    showNoInternet("NO INTERNET CONNECTION");
                                 }
                             });
                         }
-                        if (isAppStopped) {
-                            break;
-                        }
+                        Thread.sleep(RELOADING_TIME_IN_MILLIS);
 
-                        Thread.sleep(5000);
+                        if(!isDisplayOn) break;
                     }
                 } catch (InterruptedException e) {
                     e.printStackTrace();
@@ -276,7 +159,7 @@ public class MainActivity extends AppCompatActivity {
                 String marked = itemObject.getString("marked");
 
                 Date date = new Date(Long.parseLong(timeInMillis));
-                String date_str = DateFormat.format("MM/dd/yyyy", date).toString();
+                String date_str = DateFormat.format("MM/dd/yyyy HH:mm", date).toString();
 
                 Boolean marked_boolean = false;
                 if (marked.equals("0")) {
@@ -353,8 +236,14 @@ public class MainActivity extends AppCompatActivity {
                     }
                     shoppingListJSON = buffer.toString();
 
-                } catch (UnknownHostException e) {
+                } catch (ConnectException e) {
                     e.printStackTrace();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            showNoInternet("404 !!! ");
+                        }
+                    });
                 } catch (MalformedURLException e) {
                     e.printStackTrace();
                 } catch (IOException e) {
@@ -373,14 +262,101 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void showNoInternet() {
-        editText_new_item.setText("No Internet");
-        editText_new_item.setBackgroundColor(Color.LTGRAY);
+    private void showNoInternet(String msg) {
+        TableLayout tableLayout = findViewById(R.id.tableLayoutContents);
+
+        // clear table when reloading
+        if (tableLayout.getChildCount() != 0) {
+            tableLayout.removeAllViews();
+            Log.d(LOG_TAG, "tableLayout all children removed");
+        }
+
+        TableRow tableRow = new TableRow(this);
+        TextView tv = new TextView(this);
+        tv.setText(msg);
+        tv.setTextColor(Color.RED);
+        tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+        tv.setGravity(Gravity.CENTER);
+        tableRow.addView(tv);
+        tableLayout.addView(tableRow);
     }
 
-    private void showInternet() {
+    public void onButtonAddClicked(View v) {
+        editText_new_item.clearFocus();
+        newItem = editText_new_item.getText().toString();
         editText_new_item.setText("");
-        editText_new_item.setBackgroundColor(Color.WHITE);
+
+        Thread communicationThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    new URL(api_url + "/?command=insert&name=" + newItem).openStream();
+
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        communicationThread.start();
+
+        // *** wait until the communicationThread to get transportsJsonStr from url
+        try {
+            communicationThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        loadTable();
     }
 
+    public void onButtonDeleteClicked(View v) {
+        Thread communicationThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    new URL(api_url + "/?command=delete").openStream();
+
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        communicationThread.start();
+
+        // *** wait until the communicationThread to get transportsJsonStr from url
+        try {
+            communicationThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        loadTable();
+    }
+
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(LOG_TAG, "onStop() called");
+        super.onStop();
+        isDisplayOn = false;
+        Log.d(LOG_TAG, "onStop() ended");
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(LOG_TAG, "onDestroy() called");
+        super.onDestroy();
+        unregisterReceiver(connectivityReceiver);
+        Log.d(LOG_TAG, "onDestroy() ended");
+    }
 }
